9beab5f7e913f2ef2a8a3047046a524d3007c3b8
AAGaming <aa@mail.catvibers.me>
2022-06-10 16:56:39 -0400

    feat(components): remove HorizontalFocus, add Focusable

--- src/deck-components/Focusable.tsx
+++ src/deck-components/Focusable.tsx
@@ -1,15 +1,19 @@
-import { ReactNode, VFC } from "react";
+import { HTMLAttributes, ReactNode, VFC } from "react";
 import { findModuleChild } from "../webpack";
 
-export interface FocusableProps {
-    children: ReactNode;
+export interface FocusableProps extends HTMLAttributes<HTMLDivElement> {
+  children: ReactNode;
+  "flow-children"?: string;
+  focusClassName?: string;
+  focusWithinClassName?: string;
+  onActivate?: () => void;
+  onCancel?: () => void;
 }
 
-export const HorizontalFocus = findModuleChild(m => {
-    if (typeof m !== "object") return undefined;
-    for (let prop in m) {
-      if (m[prop]?.toString()?.includes('"children","alignItems","spacing"')) {
-        return m[prop];
-      }
-    }
+export const Focusable = findModuleChild((m) => {
+  if (typeof m !== 'object') return undefined;
+  for (let prop in m) {
+    if (m[prop]?.render?.toString()?.includes('["flow-children","onActivate","onCancel","focusClassName",'))
+      return m[prop];
+  }
 }) as VFC<FocusableProps>;
