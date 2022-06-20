db64f3472542b080b1d470c6b8d7aa441db0bfe6
AAGaming <aa@mail.catvibers.me>
2022-06-20 17:35:56 -0400

    feat(utils): add sleep util

--- src/utils.ts
+++ src/utils.ts
@@ -89,3 +89,7 @@ export function getReactInstance(o: HTMLElement | Element | Node) {
 export function joinClassNames(...classes: string[]): string {
     return classes.join(" ");
 }
+
+export function sleep(ms: number) {
+    return new Promise(res => setTimeout(res, ms));
+}
