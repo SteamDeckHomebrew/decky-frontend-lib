8a2baa8a299e57b83938e44030a06968eaedac7a
Lukas Senionis <warliukz@gmail.com>
2022-11-22 00:02:02 +0200

    feat(libraryInitializer): add export to index

--- src/deck-components/index.ts
+++ src/deck-components/index.ts
@@ -7,6 +7,7 @@ export * from './Field';
 export * from './Focusable';
 export * from './FocusRing';
 export * from './FooterLegend';
+export * from './LibraryInitializer';
 export * from './Menu';
 export * from './Modal';
 export * from './Panel';
