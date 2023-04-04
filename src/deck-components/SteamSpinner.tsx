b8ddf3d927401d04c0fde8ebc8960639369b8ad3
AAGaming <aa@mail.catvibers.me>
2023-04-03 23:15:30 -0400

    fix(SteamSpinner): oh apparently the class was moved outside the component for some reason??????

--- src/deck-components/SteamSpinner.tsx
+++ src/deck-components/SteamSpinner.tsx
@@ -5,7 +5,9 @@ import { findModuleChild } from '../webpack';
 export const SteamSpinner = findModuleChild((m) => {
   if (typeof m !== 'object') return undefined;
   for (let prop in m) {
-    if (m[prop]?.toString()?.includes('Steam Spinner') && m[prop]?.toString()?.includes('PreloadThrobber'))
+    if (
+      m[prop]?.toString?.()?.includes('Steam Spinner') && m[prop]?.toString?.()?.includes('src')
+    )
       return m[prop];
   }
 }) as FC<SVGAttributes<SVGElement>>;
