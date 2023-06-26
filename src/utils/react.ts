9128c1e7dadb98a8926d3dba9907a01cc78d90cf
Travis Lane <63308171+Tormak9970@users.noreply.github.com>
2023-06-26 11:29:21 -0500

    fix: add patch indicator to prevent crashes (#88)

--- src/utils/react.ts
+++ src/utils/react.ts
@@ -46,13 +46,21 @@ export function fakeRenderComponent(fun: Function, customHooks: any = {}): any {
 }
 
 export function wrapReactType(node: any, prop: any = 'type') {
-  return (node[prop] = { ...node[prop] });
+  if (node[prop]?.__DECKY_WRAPPED) {
+    return node[prop];
+  } else {
+    return (node[prop] = { ...node[prop], __DECKY_WRAPPED: true });
+  }
 }
 
 export function wrapReactClass(node: any, prop: any = 'type') {
-  const cls = node[prop];
-  const wrappedCls = class extends cls {};
-  return (node[prop] = wrappedCls);
+  if (node[prop]?.__DECKY_WRAPPED) {
+    return node[prop];
+  } else {
+    const cls = node[prop];
+    const wrappedCls = class extends cls { static __DECKY_WRAPPED = true; };
+    return (node[prop] = wrappedCls);
+  }
 }
 
 export function getReactInstance(o: HTMLElement | Element | Node) {
