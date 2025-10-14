93613551525acbdbd04c836cdf3218636ad48dd6
njzydark <njzydark@gmail.com>
2025-10-14 18:44:48 +0000

    fix(Focusable): fixes for oct 13 beta

--- src/components/Focusable.ts
+++ src/components/Focusable.ts
@@ -16,6 +16,8 @@ export interface FocusableProps extends HTMLAttributes<HTMLDivElement>, FooterLe
 
 const focusableRegex = createPropListRegex(["flow-children", "onActivate", "onCancel", "focusClassName", "focusWithinClassName"]);
 
-export const Focusable = findModuleExport((e: Export) =>
-  e?.render?.toString && focusableRegex.test(e.render.toString())
+export const Focusable = findModuleExport((e: Export) => {
+  const code: string | undefined = e?.render?.toString?.() || e?.toString?.();
+  return typeof code === 'string' && focusableRegex.test(code);
+}
 ) as FC<FocusableProps & RefAttributes<HTMLDivElement>>;
