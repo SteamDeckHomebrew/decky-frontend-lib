9fb2ce58bb3b24ece933837b5bb5b5eea889923b
AAGaming <aagaming@riseup.net>
2026-06-05 21:45:24 -0400

    fix(ErrorBoundary): support new steam beta

--- src/components/ErrorBoundary.ts
+++ src/components/ErrorBoundary.ts
@@ -2,5 +2,5 @@ import { FC, PropsWithChildren } from "react";
 import { findModuleExport } from "../webpack";
 
 export const ErrorBoundary = findModuleExport(
-    (e) => e.InstallErrorReportingStore && e?.prototype?.Reset && e?.prototype?.componentDidCatch,
+    (e) => e?.prototype?.Reset && e?.prototype?.componentDidCatch && e.prototype?.render?.toString().includes("lastErrorKey"),
 ) as FC<PropsWithChildren>; // Actually a class but @types/react is broken lol
